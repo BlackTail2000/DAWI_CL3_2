ec1fae6f6f6c60f5d8c646cc53fe7232b2b52c88
BlackTail2000 <carlosgabrielbacamanrique@hotmail.com>
2024-05-10 09:04:45 -0500

    Componente 'venta-computadoras' Generado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { VentaComputadorasComponent } from './venta-computadoras/venta-computadoras.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "venta-computadoras", component: VentaComputadorasComponent}
+];
