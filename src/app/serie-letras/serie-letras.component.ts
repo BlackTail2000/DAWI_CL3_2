7225990a0157c99134cbff44233d4837c6a4abba
Fernandocastv <fernando2044@castillo@gmail.com>
2024-05-11 08:01:26 -0500

    metodos

--- src/app/serie-letras/serie-letras.component.ts
+++ src/app/serie-letras/serie-letras.component.ts
@@ -10,9 +10,52 @@ import { Component } from '@angular/core';
 export class SerieLetrasComponent {
 
   numeroLetras: NumeroLetras[] = []
-}
 
-type NumeroLetras = {
-  numero: number,
-  letra: string
-}
+  numeroLetra = {
+    numero: 0,
+    letra: ""
+  }
+
+  mostrarResultado: boolean = false;
+
+  imprimirLetra(limite: number): void {
+
+    this.numeroLetras = [];
+
+    if (this.numeroLetra.numero < 1 || this.numeroLetra.numero > 27) {
+      alert("No se pueden ingresar Números fuera del rango de valores.");
+          return;
+    } else {
+
+      for(let i = 1; i <= limite; i++) {
+        let letra: string;
+        if(i === 27) {
+           letra = "Ñ";
+        } else {
+          letra = this.convertirNumeroALetra(i);
+        }
+
+        this.numeroLetras.push({ numero: i, letra: letra});
+
+
+  }
+    this.mostrarResultado = true;
+    }
+      
+  }
+
+  convertirNumeroALetra(numero: number) : string {
+    if(numero < 1 || numero > 27) {
+      return "Número fuera de rango";
+    }
+    if(numero === 27) {
+      return "Ñ";
+    }
+    return String.fromCharCode(64 + numero);
+  }
+  }
+
+  type NumeroLetras = {
+    numero: number,
+    letra: string
+  }
