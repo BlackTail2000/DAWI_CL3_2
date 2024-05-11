1323255d9c6f1bb6c72c14e320348971d477bac1
JulioMS2003 <julimanchay4862@gmail.com>
2024-05-11 03:18:01 -0500

    configurando ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { VentaComputadorasComponent } from './venta-computadoras/venta-computadoras.component';
+import { TiposTriangulosComponent } from './tipos-triangulos/tipos-triangulos.component';
 
 export const routes: Routes = [
-    {path: "venta-computadoras", component: VentaComputadorasComponent}
+    {path: "venta-computadoras", component: VentaComputadorasComponent},
+    {path: "tipos-triangulos", component: TiposTriangulosComponent}
 ];
--- src/app/tipos-triangulos/tipos-triangulos.component.ts
+++ src/app/tipos-triangulos/tipos-triangulos.component.ts
@@ -2,3 +2,18 @@ import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+@Component({
+  selector: 'app-tipos-triangulos',
+  standalone: true,
+  imports: [
+    FormsModule,
+    DatePipe,
+    CurrencyPipe
+  ],
+  templateUrl: './tipos-triangulos.component.html',
+  styleUrl: './tipos-triangulos.component.scss'
+})
+export class TiposTriangulosComponent {
+
+  
+}
