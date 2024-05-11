724187fb019eb231b15437a4a955b816a3ce096d
Fernandocastv <fernando2044@castillo@gmail.com>
2024-05-11 07:19:59 -0500

    componente serie-letras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { VentaComputadorasComponent } from './venta-computadoras/venta-computadoras.component';
 import { EstadosAguaComponent } from './estados-agua/estados-agua.component';
 import { TiposTriangulosComponent } from './tipos-triangulos/tipos-triangulos.component';
+import { SerieLetrasComponent } from './serie-letras/serie-letras.component';
 
 export const routes: Routes = [
     {path: "estados-agua", component: EstadosAguaComponent},
     {path: "venta-computadoras", component: VentaComputadorasComponent},
     {path: "tipos-triangulos", component: TiposTriangulosComponent},
+    {path: "serie-letras", component: SerieLetrasComponent}
 ];
---
+++ src/app/serie-letras/serie-letras.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-serie-letras',
+  standalone: true,
+  imports: [],
+  templateUrl: './serie-letras.component.html',
+  styleUrl: './serie-letras.component.scss'
+})
+export class SerieLetrasComponent {
+
+}
