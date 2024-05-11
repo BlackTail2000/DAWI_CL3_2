0edbec9f41d1dbc7b07b8286460692290c974d10
JulioMS2003 <julimanchay4862@gmail.com>
2024-05-11 03:09:49 -0500

    completando formulario y tabla

--- src/app/tipos-triangulos/tipos-triangulos.component.ts
+++ src/app/tipos-triangulos/tipos-triangulos.component.ts
@@ -1,12 +1,4 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-@Component({
-  selector: 'app-tipos-triangulos',
-  standalone: true,
-  imports: [],
-  templateUrl: './tipos-triangulos.component.html',
-  styleUrl: './tipos-triangulos.component.scss'
-})
-export class TiposTriangulosComponent {
-
-}
