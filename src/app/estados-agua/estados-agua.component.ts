2cf3d0224d125a92fead8092648c30680cf20bd8
Frutilupy22 <camiladiaz_0322@hotmail.com>
2024-05-10 23:31:04 -0500

    variables

--- src/app/estados-agua/estados-agua.component.ts
+++ src/app/estados-agua/estados-agua.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-estados-agua',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './estados-agua.component.html',
   styleUrl: './estados-agua.component.scss'
 })
 export class EstadosAguaComponent {
-
+  estadoAgua = {
+    temperatura: 0,
+    resultado: ""
+  }
+  mostrarResultado = false;
 }
